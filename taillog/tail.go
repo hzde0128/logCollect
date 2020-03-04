3bb5f700d0cabe5e853684ef7ff398d14411b120
hzde0128 <hzde0128@live.cn>
2020-03-04 19:37:21 +0800

    实现热更新配置

--- taillog/tail.go
+++ taillog/tail.go
@@ -1,6 +1,7 @@
 package taillog
 
 import (
+	"context"
 	"fmt"
 	"logAgent/kafka"
 
@@ -12,12 +13,18 @@ type TailTask struct {
 	path     string
 	topic    string
 	instance *tail.Tail
+	// 为了能实现退出t.run()
+	ctx        context.Context
+	cancelFunc context.CancelFunc
 }
 
 func NewTailTask(path, topic string) (tailObj *TailTask) {
+	ctx, cancel := context.WithCancel(context.Background())
 	tailObj = &TailTask{
-		path:  path,
-		topic: topic,
+		path:       path,
+		topic:      topic,
+		ctx:        ctx,
+		cancelFunc: cancel,
 	}
 	tailObj.init() // 根据路径去打开对应的日志
 	return
@@ -44,10 +51,14 @@ func (t *TailTask) init() {
 func (t *TailTask) run() {
 	for {
 		select {
+		case <-t.ctx.Done():
+			fmt.Printf("tail task:%s_%s 结束了...\n", t.path, t.topic)
+			return
 		case line := <-t.instance.Lines: // 从tailObj的通道中一行一行的读取日志数据
 			// 3.2 发往Kafka
 			//kafka.SendToKafka(t.topic, line.Text) // 函数调用函数
 			// 先把日志数据发到一个通道中
+			fmt.Printf("get log data from %s success, log:%v\n", t.path, line.Text)
 			kafka.SendToChan(t.topic, line.Text)
 			// kafka那个包中有单独的goroutine去取日志数据发到kafka
 		}
--- taillog/tail_mgr.go
+++ taillog/tail_mgr.go
@@ -21,11 +21,13 @@ func Init(logEntryConf []*etcd.LogEntry) {
 		newConfChan: make(chan []*etcd.LogEntry), // 无缓冲区的通道
 	}
 
-	// 遍历配置项
 	for _, logEntry := range logEntryConf {
 		//conf: *etcd.LogEntry
 		//logEntry.Path： 要收集的日志文件的路径
-		NewTailTask(logEntry.Path, logEntry.Topic)
+		// 初始化的时候起了多少个tailtask 都要记下来，为了后续判断方便
+		tailObj := NewTailTask(logEntry.Path, logEntry.Topic)
+		mk := fmt.Sprintf("%s_%s", logEntry.Path, logEntry.Topic)
+		taskMgr.taskMap[mk] = tailObj
 	}
 	go taskMgr.run()
 }
@@ -36,9 +38,35 @@ func (t *tailLogMgr) run() {
 	for {
 		select {
 		case newConf := <-t.newConfChan:
-			// 1.配置新增
-			// 2.配置删除
-			// 3.配置变更
+			for _, conf := range newConf {
+				mk := fmt.Sprintf("%s_%s", conf.Path, conf.Topic)
+				_, ok := t.taskMap[mk]
+				if ok {
+					// 原来就有，不需要操作
+					continue
+				} else {
+					// 新增的
+					tailObj := NewTailTask(conf.Path, conf.Topic)
+					t.taskMap[mk] = tailObj
+				}
+			}
+			// 2. 配置删除
+			// 找出原来t.logEntry有，但是newConf中没有的，要删掉
+			for _, c1 := range t.logEntry { // 从原来的配置中依次拿出配置项
+				isDelete := true
+				for _, c2 := range newConf { // 去新的配置中逐一进行比较
+					if c2.Path == c1.Path && c2.Topic == c1.Topic {
+						isDelete = false
+						continue
+					}
+				}
+				if isDelete {
+					// 把c1对应的这个tailObj给停掉
+					mk := fmt.Sprintf("%s_%s", c1.Path, c1.Topic)
+					//t.tskMap[mk] ==> tailObj
+					t.taskMap[mk].cancelFunc()
+				}
+			}
 			fmt.Println("新的配置来了！", newConf)
 		default:
 			time.Sleep(time.Second)
