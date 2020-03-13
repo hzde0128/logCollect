dc05c672d4b554d70f63362aff1d4f71e554a417
hzde0128 <hzde0128@live.cn>
2020-03-13 18:41:07 +0800

    update path

--- logManager/go.mod
+++ logManager/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/mattn/go-sqlite3 v1.10.0
 	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	github.com/smartystreets/goconvey v1.6.4
 )
--- logManager/tests/default_test.go
+++ logManager/tests/default_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"runtime"
 	"path/filepath"
-	_ "logCollect/logManager/routers"
+	_ "github.com/hzde0128/logCollect/logManager/routers"
 
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
